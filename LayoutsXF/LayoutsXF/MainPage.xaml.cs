0facb1094900f84b4532c4b6ffc4fa7d1a0a34a1
Erick <erick@qisolution.com.br>
2020-01-29 01:06:14 -0200

    Tipos de Lauout do Xamarin Forms.

---
+++ LayoutsXF/LayoutsXF/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using LayoutsXF.Views.Absolute;
+using LayoutsXF.Views.Grid;
+using LayoutsXF.Views.Relative;
+using LayoutsXF.Views.Scroll;
+using LayoutsXF.Views.Stack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutsXF
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToStackPage(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new StackPage());
+        }
+
+        private void GoToScrollPage(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new ScrollPage());
+        }
+
+        private void GoToRelativePage(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new RelativePage());
+        }
+
+        private void GoToGridPage(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new GridPage());
+        }
+
+        private void GoToAbsolutePage(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new AbsolutePage());
+        }
+    }
+}
